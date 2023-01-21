924d797b58ef520eb557ce2122cf7da1550a9008
ROCKERZ109 <66381573+ROCKERZ109@users.noreply.github.com>
2023-01-21 08:08:47 +0530

    Update passenger.js

--- passenger.js
+++ passenger.js
@@ -1,5 +1,5 @@
 const { MongoClient } = require('mongodb');
-const url = 'mongodb+srv://doadmin:T971H562EDlRx04c@db-mongodb-blr1-77748-8be691c1.mongo.ondigitalocean.com/passenger?authSource=admin&replicaSet=db-mongodb-blr1-77748&tls=true';
+const url = 'mongodb+srv://doadmin:*************@db-mongodb-blr1-77748-8be691c1.mongo.ondigitalocean.com/passenger?authSource=admin&replicaSet=db-mongodb-blr1-77748&tls=true';
 const client = new MongoClient(url);
 
 async function passengers() {
